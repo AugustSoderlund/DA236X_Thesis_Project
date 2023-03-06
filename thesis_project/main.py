fa800311e3296ff8ac138efd798d6e36d9a9badd
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-06 10:47:36 +0100

    main-script for complete analysis

--- thesis_project/main.py
+++ thesis_project/main.py
@@ -1,4 +1,20 @@
+from PTC.clustering import AutoCluster
+from utils.data_reader import SinD
+import pickle
+import os
 
+
+ROOT = os.getcwd()
+
+def load_data(file: str = "sind.pkl"):
+    _f = open(ROOT + "/thesis_project/.datasets/" + file, "rb")
+    return pickle.load(_f)
 
 if __name__ == "__main__":
-    pass
+    if input("Load? (y/n)") == "y":
+        data = load_data()
+    else:
+        _sind = SinD()
+        data = _sind.data()
+    c = AutoCluster()
+    c.train(data)

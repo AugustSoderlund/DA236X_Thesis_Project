de314bb75f43f5dbde7dd8bd5eac5cbe3571f791
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-06 15:09:57 +0100

    fixed both cluster and also autocluster

--- thesis_project/PTC/clustering.py
+++ thesis_project/PTC/clustering.py
@@ -1,20 +1,30 @@
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.preprocessing import StandardScaler
+from utils.data_processor import process_data
+import numpy as np
 
 
-class AutoCluster:
-    def __init__(self, n: int = 8, classifier: str = "kmeans", eps: float = 0.3):
+class Cluster:
+    def __init__(self, n: int = 8, classifier: str = "kmeans", eps: float = 0.3, preprocess_data: bool = True):
+        self.n_c, self.eps, self.preprocess_data = n, eps, preprocess_data
         self.classifier = KMeans(n_clusters=n, n_init=10) if classifier == "kmeans" else DBSCAN(eps=eps)
         self._scaler = StandardScaler()
 
-    def train(self, data):
-        _d = self._scaler.fit_transform(data)
-        self.classifier.fit(_d)
+    def train(self, data: np.ndarray, labels: np.ndarray):
+        _data = process_data(data, int(data.shape[1]/6)) if self.preprocess_data else data
+        _d = self._scaler.fit_transform(X=_data, y=labels)
+        self.classifier.fit(X=_d, y=labels)
 
-    def predict(self, data):
-        _d = self._scaler.fit_transform(data)
+    def predict(self, data: np.ndarray):
+        _data = process_data(data, int(data.shape[1]/6)) if self.preprocess_data else data
+        _d = self._scaler.fit_transform(_data)
         return self.classifier.predict(_d)
     
+class AutoCluster(Cluster):
+    def __init__(self, n: int = 8, classifier: str = "kmeans", eps: float = 0.3, preprocess_data: bool = True):
+        super().__init__(n, classifier, eps, preprocess_data)
 
-if __name__ == "__main__":
-    classifier = AutoCluster()
+    def train(self, data: np.ndarray):
+        _data = process_data(data, int(data.shape[1]/6)) if self.preprocess_data else data
+        _d = self._scaler.fit_transform(_data)
+        self.classifier.fit(_d)

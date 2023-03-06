7f55a79557520481d5f963c96143fab56e84b974
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-06 15:09:27 +0100

    class visualization in main

--- thesis_project/main.py
+++ thesis_project/main.py
@@ -1,10 +1,10 @@
 from sklearn.model_selection import train_test_split
-from PTC.clustering import AutoCluster
-from utils.data_reader import SinD
+from utils.visualization import *
+from PTC.clustering import AutoCluster, Cluster
+from utils.data_reader import SinD, LABELS
 import numpy as np
 import pickle
 import os
-
 
 ROOT = os.getcwd()
 
@@ -17,12 +17,15 @@ def split_data(data: np.ndarray, test_size: float = 0.2):
 
 
 if __name__ == "__main__":
+    _sind = SinD()
     if input("Load? (y/n)") == "y":
         data = load_data()
     else:
-        _sind = SinD()
         data = _sind.data()
     train_data, test_data = split_data(data)
-    c = AutoCluster()
-    c.train(train_data)
+    labels = _sind.labels(train_data)
+    c = Cluster(n=len(LABELS.keys()))
+    c.train(train_data, labels)
     p = c.predict(test_data)
+    print(type(c.classifier.labels_), type(labels))
+    visualize_class(_sind.map, 0, train_data, labels, 30)

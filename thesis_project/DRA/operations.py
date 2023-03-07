885f07eaa155bf99640ba18bc842f36ade3f7ddf
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-07 11:48:49 +0100

    skeleton file for zonotope operations

---
+++ thesis_project/DRA/operations.py
@@ -0,0 +1,11 @@
+from __classes import Zonotope
+
+
+def minkowski_sum(z1: Zonotope, z2: Zonotope):
+    pass
+
+def product(z1: Zonotope, z2: Zonotope):
+    pass
+
+def linear_map(L: int, z: Zonotope):
+    pass

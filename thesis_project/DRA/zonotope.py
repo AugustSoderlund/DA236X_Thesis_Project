62a3daa01e7056782b78abf5e29bd0153b055950
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-08 14:41:59 +0100

    added matrix zonotope dataclass

--- thesis_project/DRA/zonotope.py
+++ thesis_project/DRA/zonotope.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import pypolycontain as pp
 import numpy as np
 
@@ -13,3 +14,29 @@ def zonotope(c_z: np.ndarray, G_z: np.ndarray):
     """
     assert c_z.shape[0] == G_z.shape[0]
     return pp.zonotope(x=c_z, G=G_z)
+
+
+@dataclass
+class Zonotope:
+    c_z : np.ndarray
+    G_z : np.ndarray
+
+    def __post_init__(self):
+        assert self.c_z.shape[0] == self.G_z.shape[0]
+
+@dataclass
+class MatrixZonotope:
+    # TODO: Test this class to see if it works
+    C_M : np.ndarray
+    G_M : np.ndarray
+
+    def __post_init__(self):
+        assert self.C_M.shape[0] == self.G_M.shape[0]
+        assert self.C_M.shape[1] == self.G_M[0].shape[0]
+
+
+
+if __name__ == "__main__":
+    x=np.array([1,1])
+    G=np.array([[1,0,0],[0,1,0]])
+    Zonotope(x, G)

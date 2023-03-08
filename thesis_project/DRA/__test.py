c6d2c1a0ffd1cce7267f232bb592990f30bd6dcb
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-08 14:42:04 +0100

    .

--- thesis_project/DRA/__test.py
+++ thesis_project/DRA/__test.py
@@ -4,10 +4,10 @@ import numpy as np
 
 if __name__ == "__main__":
     x=np.array([1,1])
-    G=np.array([[1,0,0],[0,1,0]]).reshape(2,3)
+    G=np.array([[1,0,1],[0,1,2]])
+    p = np.array([0,-2])
     z1 = zonotope(x,G)
-    z2 = zonotope(x+2, G)
-    z3 = minkowski_sum(z1, z2)
-    z4 = product(z1,z2)
-    z3.color=(0.9, 0.9, 0.1)
-    visualize_zonotopes([z1,z2,z3])
+    boo = is_inside(z1, p)
+    print(boo)
+    visualize_zonotopes([z1])
+    plt.show()

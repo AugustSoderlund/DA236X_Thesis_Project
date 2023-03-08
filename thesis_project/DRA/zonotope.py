ca25729180c0a9b292631ae5fee77b4a293a9444
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-08 11:39:18 +0100

    zonotope creation function

--- thesis_project/DRA/zonotope.py
+++ thesis_project/DRA/zonotope.py
@@ -1,5 +1,15 @@
-from __classes import Zonotope
+import pypolycontain as pp
 import numpy as np
 
-def zonotope(c_z: np.ndarray, ):
-    pass
+def zonotope(c_z: np.ndarray, G_z: np.ndarray):
+    """ Zonotope creation 
+
+        Parameters:
+        -----------
+        c_z : np.ndarray
+            The center of the measurement
+        G_z : np.ndarray    
+            Generator matrix for zonotope
+    """
+    assert c_z.shape[0] == G_z.shape[0]
+    return pp.zonotope(x=c_z, G=G_z)

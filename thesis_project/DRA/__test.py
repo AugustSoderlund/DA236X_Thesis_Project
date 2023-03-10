631350eb08336a6d2261a7364893f88617b3928b
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-10 08:55:27 +0100

    test script

--- thesis_project/DRA/__test.py
+++ thesis_project/DRA/__test.py
@@ -3,11 +3,16 @@ from operations import *
 import numpy as np
 
 if __name__ == "__main__":
-    x=np.array([1,1])
-    G=np.array([[1,0,1],[0,1,2]])
+    x1=np.array([0,0]).reshape(2,1)
+    x2=np.array([0,0]).reshape(2,1)
+    #x=np.concatenate((x1,x2),axis=1)
+    G1=np.array([[1,0,1],[0,1,2]])
+    G2=np.array([[1,0,5],[0,1,2]])
+    #G = np.concatenate((G1,G2),axis=1)
     p = np.array([0,-2])
-    z1 = zonotope(x,G)
-    boo = is_inside(z1, p)
-    print(boo)
-    visualize_zonotopes([z1])
-    plt.show()
+    z1 = zonotope(x1,G1)
+    z2 = zonotope(x2,G2)
+    z3 = minkowski_sum(z1,z2)
+    z2.color = [0.1,0.1,0.1]
+    z3.color=[0.9,0.9,0.2]
+    visualize_zonotopes([z1,z2,z3], show=True)

a3fe6759dbf77ccb27284323bd111e5111f15aad
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-10 10:52:50 +0100

    function for bootstrapping reachable sets

--- thesis_project/DRA/reachability.py
+++ thesis_project/DRA/reachability.py
@@ -35,6 +35,36 @@ def LTI_reachability(U_minus: np.ndarray, X_plus: np.ndarray, X_minus: np.ndarra
     M_sigma = (X_plus - M_w.x) * np.linalg.pinv(_stacked) # TODO: maybe exclude M_w.x, or find better way to represent it
     for i in range(0, N-1):
         R[i+1] = minkowski_sum(linear_map(M_sigma, product(R[i], U_k[i])), Z_w)
+    return R
+
+
+def bootstrap_RA(_args: dict, conf: np.ndarray, conf_thresh: float = 0.1, N: int = None):
+    """ Linear time-invariant reachability analysis
+
+        Parameters:
+        -----------
+        _args : dict
+            All arguments required for the LTI_reachability 
+            function for each class
+        conf : np.ndarray
+            Confidence in each class from the classification
+        conf_thresh : float (default = 0.1)
+            The threshold for the confidence for the classes 
+            that the reachability will consider
+        N : int
+            The time horizon (in time steps, should be set in 
+            accordance with the frequency of measurements)
+    """
+    _R = [0] * len(conf)
+    conf[conf < conf_thresh] = 0
+    _N = [round(N*_c) for _c in conf/sum(conf)]
+    for i,n in enumerate(_N):
+        _R[i] = LTI_reachability(*_args[i], n)[-1]
+    _z = pp.zonotope(G=np.ndarray([[0,0],[0,0]]), x=np.ndarray([0,0]))
+    for _r in _R:
+        _z = minkowski_sum(_z, _r)
+    return _z
+
 
 
 def final_constraint(map, R_k: pp.zonotope):

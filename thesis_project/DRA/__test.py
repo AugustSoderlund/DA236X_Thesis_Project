61028f6a3178e9acce5796fb961ed186baec3430
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-13 10:27:25 +0100

    .

--- thesis_project/DRA/__test.py
+++ thesis_project/DRA/__test.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     x1=np.array([0,0]).reshape(2,1)
     x2=np.array([0,0]).reshape(2,1)
     #x=np.concatenate((x1,x2),axis=1)
-    G1=np.array([[1,0,1],[0,1,2]])
-    G2=np.array([[1,0,5],[0,1,2]])
+    G1=np.array([[1,0],[0,1]])
+    G2=np.array([[0,0],[0,0]])
     #G = np.concatenate((G1,G2),axis=1)
     p = np.array([0,-2])
     z1 = zonotope(x1,G1)
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     z3 = minkowski_sum(z1,z2)
     z2.color = [0.1,0.1,0.1]
     z3.color=[0.9,0.9,0.2]
-    visualize_zonotopes([z1,z2,z3], show=True)
+    

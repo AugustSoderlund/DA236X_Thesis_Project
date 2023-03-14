43de87e231b00e9d9c1edecce074eef67a715f88
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-14 15:57:50 +0100

    moved functions to data_processor.py

--- thesis_project/main.py
+++ thesis_project/main.py
@@ -1,34 +1,13 @@
 import time
-from sklearn.model_selection import train_test_split
 from utils.visualization import *
 from DRA.operations import visualize_zonotopes, optimize_vertices
 from DRA.reachability import LTI_reachability
 from PTC.input_state import *
 from PTC.classification import DecisionTree
 from utils.data_reader import SinD
+from utils.data_processor import *
 from DRA.zonotope import zonotope
 import numpy as np
-import pickle
-import os
-
-
-ROOT = os.getcwd()
-
-def load_data(file: str = "sind.pkl") -> np.ndarray:
-    _f = open(ROOT + "/thesis_project/.datasets/" + file, "rb")
-    return pickle.load(_f)
-
-def save_data(data: np.ndarray, file: str):
-    _f = open(ROOT + "/thesis_project/.datasets/" + file, "rb")
-    pickle.dump(data, _f)
-
-def split_data(data: np.ndarray, test_size: float = 0.2):
-    return train_test_split(data, test_size=test_size)
-
-def label_data(sind: SinD, data: np.ndarray, input_len: int = 30, save: str = None) -> np.ndarray:
-    if save: save_data(data, save)
-    return sind.labels(data, input_len)
-
 
 if __name__ == "__main__":
     _sind = SinD()
@@ -47,7 +26,7 @@ if __name__ == "__main__":
     #print("Accuracy (Decision tree):    ", str(sum(true_labels==p_dt)/len(p_dt)))
     #classification_acc_per_class(true_labels, p_dt, plot=True)
     #visualize_all_classes(_sind.map, len(LABELS.keys()), train_data, p, 30)
-    #visualize_class(_sind.map, 4, test_data, true_labels, 30)
+    #visualize_class(_sind.map, 0, test_data, p_dt)
     d = separate_data_to_class(test_data, p_dt)
     c_z = np.array([test_data[0][30],  test_data[0][60]])
     G_z = np.array([[4,0,3],[0,4,-2]])
@@ -64,23 +43,6 @@ if __name__ == "__main__":
     #plt.scatter(X_p[0], X_p[1], c="r")
     R = LTI_reachability(U, X_p, X_m, z, z_w, M_w, U_k, N=30)
     R = R[-1]
-    #t = time.time()
-    #V, c_z = compute_vertices(R)
-    #print(time.time()-t)
-    #t = time.time()
-    V_opt = optimize_vertices(R)
-    #print(time.time()-t)
-    #_, ax = plt.subplots()
-    #ax.scatter(c_z.T[0], c_z.T[1], c="r")
-    #ax.scatter(V_opt.T[0], V_opt.T[1], c="g")
-    #ax.add_patch(PolygonPatch(V_opt, alpha=0.5, color="g"))
-    #plt.show()
-    #_, ax = plt.subplots()
-    #sh = alphashape.alphashape(np.array(list(zip(*R.G))), alpha=0.02)
-    #sh = sh.convex_hull.simplify(0.05)
-    #p = Polygon(sh)
-    #ax.scatter(R.G[0], R.G[1], c="r")
-    #ax.add_patch(PolygonPatch(p, alpha=0.5, color="g"))
-    #plt.show()
-    #print(R.G.shape)
-    visualize_zonotopes([R], map=_sind.map, show=True)
+    #V_opt = optimize_vertices(R)
+    R.color = [0.9,0.9,0.1]
+    visualize_zonotopes([R, z], map=_sind.map, show=True)
--- thesis_project/utils/data_processor.py
+++ thesis_project/utils/data_processor.py
@@ -1,5 +1,12 @@
+from sklearn.model_selection import train_test_split
+from PTC.input_state import *
+from data_reader import SinD
+from typing import List
 import numpy as np
+import pickle
+import os
 
+ROOT = os.getcwd()
 
 def process_data(data: np.ndarray, input_len: int):
     """ Subtract the mean from (x,y) from data
@@ -17,3 +24,61 @@ def process_data(data: np.ndarray, input_len: int):
         _x, _y = _x-np.mean(_x), _y-np.mean(_y)
         _processed_data.append(np.array([*_x, *_y, *_d[2*input_len:]]))
     return _processed_data
+
+def load_data(file: str = "sind.pkl") -> np.ndarray:
+    """ Load previously pickled data
+
+        Parameters:
+        -----------
+        file : str (default = 'sind.pkl')
+            File-name of the pickled file. Note that
+            it has to be stored in the relative dir
+            '/thesis_project/.datasets/'
+    """
+    _f = open(ROOT + "/thesis_project/.datasets/" + file, "rb")
+    return pickle.load(_f)
+
+def save_data(data: np.ndarray, file: str):
+    """ Save data into file
+    
+        Parameters:
+        -----------
+        data : np.ndarray
+            The data that will be pickled and stored
+        file : str
+            Name of the file it will be stored into
+            relative dir: '/thesis_project/.datasets/'
+    """
+    _f = open(ROOT + "/thesis_project/.datasets/" + file, "rb")
+    pickle.dump(data, _f)
+
+def split_data(data: np.ndarray, test_size: float = 0.2) -> List[np.ndarray]:
+    """ Split the data into train- and test data
+        (this function randomizes the order or the data)
+
+        Parameters:
+        -----------
+        data : np.ndarray
+            The dataset that will be splitted
+        test_size : float (default = 0.2)
+            Size of the test dataset
+    """
+    return train_test_split(data, test_size=test_size)
+
+def label_data(sind: SinD, data: np.ndarray, input_len: int = 30, save: str = None) -> np.ndarray:
+    """ Function for getting the labels for a dataset
+
+        Parameters:
+        -----------
+        sind : SinD
+            The SinD-class which calls the function
+        data : np.ndarray
+            The data to be labeled
+        input_len : int (default = 30)
+            The length for each chunk in the dataset
+        save : str (default = None)
+            Name of the file if the labels will be stored
+            (will save if not set to None)
+    """
+    if save: assert type(save) == str; save_data(data, save)
+    return sind.labels(data, input_len)

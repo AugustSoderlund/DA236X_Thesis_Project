e3d82223663a3f3840d3e768f4a7e6c981b9d78e
AugustSoderlund <carlaugustsoderlund@gmail.com>
2023-03-17 11:47:41 +0100

    product revised, reduce function implmntd

--- thesis_project/DRA/operations.py
+++ thesis_project/DRA/operations.py
@@ -1,13 +1,14 @@
 from typing import List, Union
 from matplotlib.collections import PatchCollection
 import pypolycontain as pp
+from DRA.zonotope import zonotope, matrix_zonotope
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from shapely.geometry import Point, Polygon
 from matplotlib.patches import Polygon as poly
 from scipy.spatial import ConvexHull
 import alphashape
-#from utils.map import SinD_map
 import platform
 
 if platform.system() == "Windows":
@@ -29,7 +30,7 @@ def minkowski_sum(z1: pp.zonotope, z2: pp.zonotope) -> pp.zonotope:
     assert c_z.shape[0] == G_z.shape[0]
     return pp.zonotope(x=c_z, G=G_z)
 
-def product(z1: pp.zonotope, z2: pp.zonotope) -> pp.zonotope:
+def cartesian_product(z1: pp.zonotope, z2: pp.zonotope) -> pp.zonotope:
     """ Calculates the cartesian product of two zonotopes
 
         Parameters:
@@ -37,24 +38,91 @@ def product(z1: pp.zonotope, z2: pp.zonotope) -> pp.zonotope:
         z1 : pp.zonotope
         z2 : pp.zonotope
     """
-    c_z = np.concatenate((z1.x, z2.x), axis=0)
-    _top = np.concatenate((z1.G, np.zeros(shape=z2.G.shape)), axis=1)
-    _bottom = np.concatenate((np.zeros(shape=z1.G.shape), z2.G), axis=1)
+    z1.G = z1.G.reshape(z1.G.shape[0], -1)
+    z2.G = z2.G.reshape(z2.G.shape[0], -1)
+    c_z = np.concatenate((z1.x, z2.x), axis=0).reshape(z1.x.shape[0]+z2.x.shape[0], 1)
+    _top = np.concatenate((z1.G, np.zeros(shape=(z1.G.shape[0], z2.G.shape[1]))), axis=1)
+    _bottom = np.concatenate((np.zeros(shape=(z2.G.shape[0], z1.G.shape[1])), z2.G), axis=1)
     G_z = np.concatenate((_top, _bottom), axis=0)
     assert c_z.shape[0] == G_z.shape[0]
     return pp.zonotope(x=c_z, G=G_z)
 
-def linear_map(L: np.ndarray, z: pp.zonotope) -> pp.zonotope:
+def product(f1: Union[pp.zonotope, np.ndarray], f2: Union[pp.zonotope, np.ndarray]) -> pp.zonotope:
+    """ Multiplies the zonotope with a factor (note: this is different
+        from the cartesian product AND type of f1 and f2 can't both 
+        be numpy.ndarray)
+
+        Parameters:
+        -----------
+        f1 : pp.zonotope | np.ndarray
+        f2 : pp.zonotope | np.ndarray
+    """
+    G_z = []
+    if type(f1) == np.ndarray and type(f2) == np.ndarray: raise TypeError
+    if type(f1) == type(f2):
+        c_z = np.matmul(f1.x, f2.x)
+        n = c_z.shape[0]
+        G_z1 = np.matmul(f1.x, f2.G)                                 #  --> (2x5)
+        G_z2 = np.matmul(f1.G, f2.x).reshape(n,-1)  
+        G_z3 = np.matmul(f1.G, f2.G).reshape(n,-1)
+        G_z = np.hstack((G_z1, G_z2, G_z3))
+    elif type(f2) == pp.zonotope: 
+        c_z = np.matmul(f1, f2.x) 
+        n = c_z.shape[0]
+        if len(f2.G.shape) == 1: G_z = np.matmul(f1, f2.G).reshape(f1.shape[0],-1)
+        else: G_z = np.matmul(f1, f2.G).reshape(n,-1)#G_z = [[*G_z, *np.matmul(f1, g_z)] for g_z in f2.G]
+    else:
+        c_z = np.matmul(f1.x, f2) 
+        G_z = [[*G_z, *np.matmul(g_z, f2)] for g_z in f1.G]
+    try: 
+        if c_z.shape[1] > 1: return matrix_zonotope(c_z, np.array(G_z))
+        else: return zonotope(c_z, np.array(G_z))
+    except Exception: return zonotope(c_z, np.array(G_z))
+
+
+
+    
+def create_M_w(total_size: int, Z: pp.zonotope = zonotope(np.array([0,0]), np.array([0.05,0.05]))) -> pp.zonotope:
+    """ Function to construct the matrix zonotope M_w
+
+        Parameters:
+        -----------
+        total_size : int
+            Total size of dataset X_
+        Z : pp.zonotope
+            Process noise zonotope
+            dim(Z.G) -> (dim(c_z), 1)
+    """
+    _id = 0
+    _G_w = [0] * (Z.G.shape[1] * total_size)
+    for i in range(Z.G.shape[1]):
+        _vec = Z.G[:,i].reshape(Z.G.shape[0], 1)
+        _G_w[_id] = np.concatenate((_vec, np.zeros(shape=(Z.G.shape[0], total_size-1))), axis=1)
+        for j in range(1, total_size):
+            _G_w[_id+j] = np.concatenate((_G_w[_id+j-1][:,1:], _G_w[_id+j-1][:,0].reshape(Z.G.shape[0],1)), axis=1)
+        _id = _id + j + 1
+    return matrix_zonotope(C_M=np.zeros(shape=(Z.G.shape[0], total_size)), G_M=np.array(_G_w))
+
+
+
+def linear_map(L: Union[int, np.ndarray], z: pp.zonotope, from_side: str = "left") -> pp.zonotope:
     """ Perform linear map of a zonotope
 
         Parameters:
         -----------
         L : int
         z : pp.zonotope
+        from_side : str (default = 'left')
+            Defines from which side the matrix L should be
+            multiplied (from set {'left', 'right'}) 
     """
-    if type(L) == np.ndarray: assert L.shape[1] == (z.x.shape[0] and z.G.shape[0]); _z = pp.zonotope(x=np.matmul(L,z.x), G=np.matmul(L,z.G))
+    if type(L) == np.ndarray: 
+        if from_side == "left": assert L.shape[1] == (z.x.shape[0] and z.G.shape[0]); _z = pp.zonotope(x=np.matmul(L,z.x), G=np.matmul(L,z.G))
+        elif from_side == "right": assert L.shape[0] == (z.x.shape[1] and z.G.shape[1]); _z = pp.zonotope(x=np.matmul(z.x,L), G=np.matmul(z.G,L))
+        else: print("Only 'left' and 'right' are viable args"); raise TypeError
     elif type(L) == int: _z = pp.zonotope(x=L*z.x, G=L*z.G)
     return _z
+
 
 
 def is_inside(z: pp.zonotope, point: np.ndarray) -> bool:
@@ -72,6 +140,10 @@ def is_inside(z: pp.zonotope, point: np.ndarray) -> bool:
     else: V = pp.to_V(z)
     _poly = Polygon(V).buffer(2*np.finfo(float).eps)
     return _poly.contains(Point(point))
+
+def forge_traj(x: np.ndarray, v: np.ndarray, c: int, map):
+    pass
+
 
 def optimize_vertices(z: pp.zonotope, n: int = 2, simplify: bool = True):
     """ Optimize the vertices calculations by creating an alphashape 
@@ -93,6 +165,9 @@ def optimize_vertices(z: pp.zonotope, n: int = 2, simplify: bool = True):
     """
     if use_pydrake: return pp.to_V(z)
     else:
+        z = reduce(z, 6)
+        return pp.to_V(z)
+        """ LEGACY """
         c_z = np.copy(z.x)
         for i, g_z in enumerate(z.G.T):
             c_z = np.vstack([c_z+g_z, c_z-g_z])
@@ -106,6 +181,46 @@ def optimize_vertices(z: pp.zonotope, n: int = 2, simplify: bool = True):
             return c_z[ConvexHull(c_z).vertices,:]
         except Exception as e:
             print(e)
+
+def reduce(z: pp.zonotope, order: int):
+    """ Function that reduces the order of a zonotope 
+        (NOTE: rewritten from MATLAB library 'CORA',
+            Author: Matthias Althoff)
+
+        Parameters:
+        -----------
+        z : pp.zonotope
+        order : int
+            How many generators should the reduced
+            zonotope contain
+    """
+    def __non_zero_filter(G):
+        _idx = np.argwhere(np.all(G[...,:] == 0, axis=0))
+        return np.delete(G, _idx, axis=1)
+    def __picked_generators(z: pp.zonotope, order: int):
+        c_z, G = z.x, z.G
+        G = __non_zero_filter(G)
+        d, num_gens = G.shape
+        if num_gens > d*order:
+            h = np.linalg.norm(G,1,axis=0) - np.linalg.norm(G,np.inf,axis=0)
+            n_unred = math.floor(d*(order-1))
+            n_red = num_gens - n_unred
+            _idx = np.argsort(h)[:n_red]
+            G_red = G[:,_idx]
+            _idy = np.setdiff1d(list(range(0,num_gens)), _idx)
+            G_unred = G[:,_idy]
+        else:
+            G_red = np.array([])
+            G_unred = G
+        return (c_z, G_unred, G_red)
+    def __girard(z: pp.zonotope, order: int):
+        c_z, G_unred, G_red = __picked_generators(z, order)
+        if G_red.size > 0: _G = np.diag(np.sum(abs(G_red), axis=1)); G = np.hstack((G_unred, _G))
+        else: G = G_unred
+        return zonotope(c_z=c_z, G_z=G)
+    return __girard(z, order)
+
+
 
 
 def compute_vertices(z: pp.zonotope):
@@ -164,9 +279,16 @@ def visualize_zonotopes(z: Union[List[pp.zonotope], List[np.ndarray]], map = Non
     absolute size of the figure and the zonotopes are simply plotted
     within the map
 """
-def visualize(list_of_objects: Union[List[pp.zonotope], List[np.ndarray]], ax: plt.axes = None, alpha: float = 0.8, title: str = r'pypolycontain visualization', 
-              show_vertices: bool = False, TitleSize: int = 15, FontSize: int = 15, equal_axis: bool = False, grid: bool = True,
-              N_points: int = 1000, scale_axes: bool = False):
+def _projection(P,tuple_of_projection_dimensions):
+    p_matrix=np.zeros((2,P.n))
+    p_matrix[0,tuple_of_projection_dimensions[0]]=1
+    p_matrix[1,tuple_of_projection_dimensions[1]]=1
+    return pp.affine_map( p_matrix, P)
+
+
+def visualize(list_of_objects: Union[List[pp.zonotope], List[np.ndarray]], ax: plt.axes = None, alpha: float = 0.8, 
+              title: str = r'pypolycontain visualization', show_vertices: bool = True, TitleSize: int = 15, 
+              FontSize: int = 15, equal_axis: bool = False, grid: bool = True, scale_axes: bool = False):
     r"""
     Visualization.
     
@@ -177,13 +299,19 @@ def visualize(list_of_objects: Union[List[pp.zonotope], List[np.ndarray]], ax: p
         * 
     """
     a = 0.5
+    tuple_of_projection_dimensions=[0,1]
     if type(ax)==type(None):
         _,ax=plt.subplots()
     p_list, x_all= [], np.empty((0,2))  
     for p in list_of_objects:
+        if p.n>2:
+            print('projection on ',tuple_of_projection_dimensions[0],\
+                  ' and ',tuple_of_projection_dimensions[1], 'dimensions')
+            p=_projection(p,tuple_of_projection_dimensions)
         if type(p) == pp.zonotope:
-            if use_pydrake: x = pp.to_V(p,N=N_points)
-            else: x = optimize_vertices(p)
+            x = pp.to_V(reduce(p,6))
+            #if use_pydrake: x = pp.to_V(p,N=N_points)
+            #else: x = optimize_vertices(p)
         else:
             x = p
         mypolygon = poly(x)
@@ -191,7 +319,7 @@ def visualize(list_of_objects: Union[List[pp.zonotope], List[np.ndarray]], ax: p
         x_all = np.vstack((x_all, x))
         if show_vertices:
             ax.plot(x[:,0],x[:,1],'*',color=p.color)
-    p_patch = PatchCollection(p_list, alpha=alpha)
+    p_patch = PatchCollection(p_list,color=[p.color for p in list_of_objects], alpha=alpha)
     ax.add_collection(p_patch)
     if scale_axes:
         ax.set_xlim([np.min(x_all[:,0])-a,a+np.max(x_all[:,0])])
@@ -199,8 +327,19 @@ def visualize(list_of_objects: Union[List[pp.zonotope], List[np.ndarray]], ax: p
     if grid:
         ax.grid(color=(0,0,0), linestyle='--', linewidth=0.3)
     ax.set_title(title, FontSize=TitleSize)
-    ax.set_xlabel("x_1", FontSize=FontSize)
-    ax.set_ylabel("x_2", FontSize=FontSize)
+    ax.set_xlabel("$x_1$", FontSize=FontSize)
+    ax.set_ylabel("$x_2$", FontSize=FontSize)
     if equal_axis:
         ax.axis('equal')
+
+
+class A:
+    def __init__(self):
+        pass
+
+    def __add__(self, B):
+        pass
+
+    def __mult__(self):
+        pass
 
